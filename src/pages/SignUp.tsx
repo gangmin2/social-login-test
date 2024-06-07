ce50efeda917f82abfd779b101c7de4722a2a66c
gangmin2 <min0115b@gmail.com>
2024-06-07 13:35:42 +0900

    Feat: SignUp Page 생성

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,26 @@
+import './signup.css';
+
 const SignUp = () => {
     return (
         <>
-            <h1>SignUp</h1>
+            <div className='container'>
+                <div className='signup-container'>
+                    <div className='logo'>3Ever</div>
+                    <form className='form-container' action="">
+                        <label htmlFor="">아이디</label>
+                        <input type="text" placeholder='아이디' />
+
+                        <label htmlFor="">닉네임</label>
+                        <input type="text" placeholder='닉네임' />
+
+                        <label htmlFor="">비밀번호</label>
+                        <input type="password" placeholder='비밀번호' />
+                        <input type="password" placeholder='비밀번호 확인' />
+
+                        <button className='signup-button' type="submit">회원가입</button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
