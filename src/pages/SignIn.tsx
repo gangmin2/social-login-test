b3a9b75a9dafcaa5210154af0930af782fc111f9
gangmin2 <min0115b@gmail.com>
2024-06-07 13:26:03 +0900

    Feat: SignIn Page 생성

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,37 @@
+import { Link, useNavigate } from 'react-router-dom';
+import './signin.css';
+
 const SignIn = () => {
+    const nav = useNavigate();
+
+    const onClickSocialButton = (path:string) => {
+        nav('authorized' + path);
+    } 
     return (
         <>
-            <h1>SignIn</h1>
+            <div className='container'>
+                <div className='signin-container'>
+                    <div className='logo'>3Ever</div>
+                    <form className='form-container' action="">
+                        <input type="text" placeholder='아이디' />
+                        <input type="password" placeholder='비밀번호' />
+                        <button type="submit">로그인</button>
+                    </form>
+                    <div className='links-container'>
+                        <Link to="/signup">회원가입 하러가기</Link>
+                        <Link to=''>아이디 비밀번호 찾기</Link>
+                    </div>
+                    <div className='devider'>
+                        <div className='devider-border'></div>
+                        <span>또는</span>
+                        <div className='devider-border'></div>
+                    </div>
+                    <div className='social-login-buttons'>
+                        <button onClick={() => onClickSocialButton('/kakao')} className='bg-kakao'>카카오계정으로 로그인</button>
+                        <button onClick={() => onClickSocialButton('/naver')} className='bg-naver'>네이버로 로그인</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
