6a14069f4f038275a54c836d896ecade6b6303c1
gangmin2 <min0115b@gmail.com>
2024-06-07 12:35:41 +0900

    Feat: React Router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Landing from "./pages/Landing"
+import SignIn from './pages/SignIn.tsx'
+import SignUp from './pages/SignUp.tsx'
+import NotFound from './pages/NotFound.tsx'
+
 function App() {
   return (
     <>
-      <h1>hi</h1>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App;
